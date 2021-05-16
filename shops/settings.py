938e34dcf455c0355f589230c4388f3e696a9a30
garyhxfang <garyhxfang@163.com>
2021-05-16 16:58:11 +0800

    Update settings.py

--- shops/settings.py
+++ shops/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'shops.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shops',
+        'USER': 'garyhxfang',
+        'PASSWORD': 'shangqingbb1201',
     }
 }
 
